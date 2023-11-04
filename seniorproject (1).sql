8a4651294f42d38ccc53c0caa85a274750f81a97
CaptianSiix <53707930+CaptianSiix@users.noreply.github.com>
2023-11-03 22:55:33 -0400

    Add files via upload

---
+++ seniorproject (1).sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 04, 2023 at 03:54 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `seniorproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `postsId` int(11) NOT NULL,
+  `postsTitle` varchar(255) NOT NULL,
+  `postsBody` varchar(255) NOT NULL,
+  `postsTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `postsusersUid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `usersID` int(11) NOT NULL,
+  `usersName` varchar(255) NOT NULL,
+  `usersEmail` varchar(255) NOT NULL,
+  `usersUid` varchar(255) NOT NULL,
+  `usersPwd` varchar(255) NOT NULL,
+  `usersAdmin` enum('Viewer','Admin') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`usersID`, `usersName`, `usersEmail`, `usersUid`, `usersPwd`, `usersAdmin`) VALUES
+(1, 'Sean ', 'ax@l.com', 'Siixa', '$2y$10$RAcK1nLM1RdW/nfWInAn2uU/hAdzPwZTHBxoDA3d7XsXkSvUQ6HHu', 'Admin'),
+(2, 'Sean ', 'S@1l.com', 'Siixb', '$2y$10$nmKehJedq6Cg26Ibyp2U2.0C1JxjeCY5OynqcYZ0eF9.oFrzTfTFO', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`postsId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`usersID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `usersID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
